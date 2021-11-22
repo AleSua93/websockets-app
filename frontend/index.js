c6f805a8d7b69e75735598772d828780a82478cc
Alejandro Suarez <alejandronicolassuarez@hotmail.com>
2021-11-21 23:53:53 -0300

    refactor: clean up frontend code

--- frontend/index.js
+++ frontend/index.js
@@ -2,14 +2,4 @@ const ws = new WebSocket('ws://localhost:8000', 'json');
 
 ws.addEventListener('open', () => {
   console.log('Connection open!');
-
-  const data = { message: 'Hello from the client!' }
-  const json = JSON.stringify(data);
-  ws.send(json);
-});
-
-ws.addEventListener('message', event => {
-  console.log('Message received!');
-  const data = JSON.parse(event.data);
-  console.log(data);
 });
